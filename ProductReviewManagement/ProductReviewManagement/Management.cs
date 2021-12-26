a8e5f5a4eda44e66d3b8be59094ccf2fb9f8becc
Hitesh <hiteshspatil99@gmail.com>
2021-12-26 17:06:18 +0530

    [Hitesh] add : Retrieve Records Where Review Nice

--- ProductReviewManagement/ProductReviewManagement/Management.cs
+++ ProductReviewManagement/ProductReviewManagement/Management.cs
@@ -107,6 +107,27 @@ namespace ProductReviewManagement
                 Console.WriteLine(list.ProductId + "   " + list.AverageRatting);
             }
         }
+        public static void RetrieveRecordsWhereReviewNice(List<ProductReview> ProductReviewList)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("ProductId").DataType = typeof(Int32);
+            dataTable.Columns.Add("UserId").DataType = typeof(Int32);
+            dataTable.Columns.Add("Ratting").DataType = typeof(Int32);
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("IsLike").DataType = typeof(bool);
+            foreach (var item in ProductReviewList)
+            {
+                dataTable.Rows.Add(item.ProductId, item.UserId, item.Ratting, item.Review, item.IsLike);
+            }
+            var productTable = from products in dataTable.AsEnumerable()
+                               where products.Field<string>("Review") == "nice"
+                               select products;
+
+            foreach (DataRow product in productTable)
+            {
+                Console.WriteLine(product.Field<int>("ProductId") + " " + product.Field<int>("UserId") + " " + product.Field<int>("Ratting") + " " + product.Field<string>("Review") + " " + product.Field<bool>("IsLike"));
+            }
+        }
 
     }
 }
